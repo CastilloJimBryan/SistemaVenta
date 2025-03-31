ee5aa1d2650535598386241a93bbc69362fa2b4d
Bryan Castillo <jimbrayan@hotmail.com>
2025-03-31 09:08:51 -0300

    servicio

--- Servicio/Encriptacion.cs
+++ Servicio/Encriptacion.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Servicio
 {
-    class Encriptacion
+    public class Encriptacion
     {
+        public void Hash(string text)
+        {
+            using (SHA256 encry= SHA256.Create())
+            {
+                byte[ ] b=Encoding.UTF8.GetBytes(text);
+                byte[] bytehash = encry.ComputeHash(b); 
+            }
+        }
     }
 }
