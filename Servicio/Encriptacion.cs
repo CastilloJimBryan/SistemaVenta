58386e9773570aeaa8ff35d41cce765a1829bd60
Bryan castillo <jimbrayan@hotmail.com>
2025-04-01 12:03:20 -0300

    Se mejoro encriptacion

--- Servicio/Encriptacion.cs
+++ Servicio/Encriptacion.cs
@@ -9,12 +9,19 @@ namespace Servicio
 {
     public class Encriptacion
     {
-        public void Hash(string text)
+        public static string Hash(string text)
         {
             using (SHA256 encry= SHA256.Create())
             {
                 byte[ ] b=Encoding.UTF8.GetBytes(text);
-                byte[] bytehash = encry.ComputeHash(b); 
+                byte[] bytehash = encry.ComputeHash(b);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < bytehash.Length; i++)
+                {
+                    sb.Append(bytehash[i].ToString("x2")); // Formato hexadecimal
+                }
+                return sb.ToString();
             }
         }
     }
