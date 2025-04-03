53adc8b5a84c34bb8b2274d41bd2814199268435
Bryan Castillo <jimbrayan@hotmail.com>
2025-04-03 12:48:48 -0300

    se agrego funcionalidad de bitacora listar

---
+++ BL/BitacoraBL.cs
@@ -0,0 +1,23 @@
+﻿using BE;
+using DA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class BitacoraBL
+    {
+        BitacoraDAL bitacoraDAL;
+        public BitacoraBL()
+        {
+            bitacoraDAL = new BitacoraDAL();
+        }
+        public List<Bitacora> ListadoBitacora()
+        {
+            return bitacoraDAL.ListadoBitacora();
+        }
+    }
+}
---
+++ DA/BitacoraDAL.cs
@@ -0,0 +1,48 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA
+{
+    public class BitacoraDAL
+    {
+        SqlConnection connection => new SqlConnection(ConfigurationManager.ConnectionStrings["BDA"].ConnectionString);
+        public List<Bitacora> ListadoBitacora()
+        {
+            using (SqlConnection con = connection)
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection= con;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "SELECT Bitacora.Id,Actividad,Fecha,Usuario.Id as UsuarioId,Usuario.Nombre as Usuario" +
+                        " FROM Bitacora" +
+                        " INNER JOIN Usuario ON Bitacora.UsuarioId=Usuario.Id";
+                    List<Bitacora> list=new List<Bitacora>();
+                    using (SqlDataReader dr=cmd.ExecuteReader())
+                    {
+                        while(dr.Read())
+                        {
+                            list.Add(new Bitacora
+                            {
+                                Id = int.Parse(dr["Id"].ToString()),
+                                UsuarioId = int.Parse(dr["UsuarioId"].ToString()),
+                                Usuario = dr["Usuario"].ToString(),
+                                Actividad = dr["Activida"].ToString(),
+                                Fecha = DateTime.Parse(dr["Fecha"].ToString())
+                            });
+                        }
+                        return list;
+                    }
+                }
+            }
+        }
+    }
+}
