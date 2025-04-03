939a821bebb53b4c6af2bb3070606fb372a2b6d1
Bryan Castillo <jimbrayan@hotmail.com>
2025-04-03 12:21:27 -0300

    se agrego clase bitacora

---
+++ BE/Bitacora.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Bitacora:Entity
+    {
+		private DateTime fecha;
+
+		public DateTime Fecha
+		{
+			get { return fecha; }
+			set { fecha = value; }
+		}
+		private string usuario;
+
+		public string Usuario
+		{
+			get { return usuario; }
+			set { usuario = value; }
+		}
+		private int usuarioId;
+
+		public int UsuarioId
+		{
+			get { return usuarioId; }
+			set { usuarioId = value; }
+		}
+
+		private string actividad;
+
+		public string Actividad
+		{
+			get { return actividad; }
+			set { actividad = value; }
+		}
+
+
+	}
+}
