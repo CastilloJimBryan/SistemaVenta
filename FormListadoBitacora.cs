3d3031837618c1d79859433bb2397f2988db188d
Bryan Castillo <jimbrayan@hotmail.com>
2025-04-04 09:47:36 -0300

    se termino bitacora con filtrado

--- FormListadoBitacora.cs
+++ FormListadoBitacora.cs
@@ -14,8 +14,10 @@ namespace Sistema_Venta
 {
     public partial class FormListadoBitacora: Form
     {
+        BitacoraBL bitacoraBL;
         public FormListadoBitacora()
         {
+            bitacoraBL = new BitacoraBL();
             InitializeComponent();
         }
 
@@ -33,6 +35,7 @@ namespace Sistema_Venta
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridView1.ReadOnly = true;
             dataGridView1.MultiSelect = false;
+            dateTimePicker1.Visible = false;
 
             Actualizar();
         }
@@ -43,6 +46,62 @@ namespace Sistema_Venta
             {
                 dataGridView1.Rows.Add(b.Id, b.UsuarioId, b.Usuario, b.Actividad, b.Fecha);
             }
+        }
+
+        void filtrar()
+        {
+            List<Bitacora> listad=bitacoraBL.ListadoBitacora();
+
+            if(rbusuario.Checked)
+            {
+                listad=listad.FindAll(u=>u.Usuario.StartsWith(textBox1.Text, StringComparison.OrdinalIgnoreCase));
+            }
+            else if (rbactividad.Checked)
+            {
+                listad = listad.FindAll(a => a.Actividad.StartsWith(textBox1.Text, StringComparison.OrdinalIgnoreCase));
+            }
+            if (rbfecha.Checked)
+            {
+                DateTime fechaSeleccionada = dateTimePicker1.Value.Date;
+                listad=listad.FindAll(f=>f.Fecha.Date==fechaSeleccionada);
+            }
+
+            dataGridView1.Rows.Clear();
+
+            foreach(var b in listad)
+            {
+                dataGridView1.Rows.Add(b.Id, b.UsuarioId, b.Usuario, b.Actividad, b.Fecha);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            filtrar();
+        }
+
+        private void rbfecha_CheckedChanged(object sender, EventArgs e)
+        {
+            dateTimePicker1.Visible = true;
+            textBox1.Visible = false;
+        }
+
+        private void rbusuario_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            dateTimePicker1.Visible = false;
+            textBox1.Visible = true;
+        }
+
+        private void rbactividad_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            dateTimePicker1.Visible = false;
+            textBox1.Visible = true;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            filtrar();
         }
     }
 }
